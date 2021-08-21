da532ec2a2a4e241e7a7ff033c6b2d0cebb76b3f
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-22 00:02:53 +0300

    Add news tests for the news class

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,70 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NewsTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getNewsTitleReturnCorrectNewsTitle() throws Exception{
+        News testNews = setNews();
+        assertEquals("Daily Nation", testNews.getNewsTitle());
+    }
+
+
+    @Test
+    public void  getNewsContentReturnNewsContent() throws Exception{
+        News testNews = setNews();
+        assertEquals("BBI Agenda", testNews.getNewsContent());
+    }
+
+    @Test
+    public void  getDepartmentIdReturnDepartmentId() throws Exception{
+        News testNews = setNews();
+        assertEquals(1, testNews.getDepartmentId());
+    }
+
+    @Test
+    public void setNewsTitLeSetsCorrectNewsTitle() throws Exception {
+        News testNews = setNews();
+        testNews.setNewsTitle("Taifa Leo");
+        assertNotEquals("Daily Nation",testNews.getNewsTitle());
+    }
+
+    @Test
+    public void setNewsContentCorrectNewsContent() throws Exception {
+        News testNews = setNews();
+        testNews.setNewsContent("Elections");
+        assertEquals("BBI Agenda", testNews.getNewsContent());
+    }
+
+
+    @Test
+    public void setDepartmentIdCorrectDepartmentId()throws Exception{
+        News testNews = setNews();
+        testNews.setDepartmentId(2);
+        assertEquals(1, testNews.getDepartmentId());
+    }
+
+
+
+
+    public News setNews (){
+        return new News("Daily Nation", "BBI Agenda",  1);
+    }
+
+    public News setAltNews (){
+        return new News("Daily Nation", "BBI Agenda",  1);
+    }
 }
--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -18,52 +18,52 @@ public class UserTest {
 
     @Test
     public void getUsernameReturnCorrectUserName() throws Exception{
-        User testUser = setupAltUser();
+        User testUser = setUser();
         assertEquals("Fredrick Angose", testUser.getUserName());
     }
 
     @Test
     public  void getUserCompanyPositionReturnUserCompanyPosition() throws Exception{
-        User testUser = setUpUser();
+        User testUser = setUser();
         assertEquals("Manager", testUser.getUserCompanyPosition());
     }
 
     @Test
     public void  getUserCompanyRoleReturnUserCompanyRole() throws Exception{
-        User testUser = setUpUser();
+        User testUser = setUser();
         assertEquals("Developer", testUser.getUserCompanyRole());
     }
 
     @Test
     public void  getDepartmentIdReturnDepartmentId() throws Exception{
-        User testUser = setUpUser();
+        User testUser = setUser();
         assertEquals(1, testUser.getDepartmentId());
     }
 
     @Test
     public void setUserNameSetsCorrectUserName() throws Exception {
-        User testUser = setupUser();
+        User testUser = setUser();
         testUser.setUserName("Daniel Owino");
         assertNotEquals("Fredrick Angose",testUser.getUserName());
     }
 
     @Test
     public void setUserCompanyPositionCorrectUserCompanyPosition() throws Exception {
-       User testUser = setUpUser();
+       User testUser = setUser();
        testUser.setUserCompanyPosition("Secretary");
        assertEquals("Manager", testUser.getUserCompanyPosition());
     }
 
     @Test
     public void setUserCompanyRoleCorrectUserCompanyRole() throws Exception{
-        User testUser = setupUser();
+        User testUser = setUser();
         testUser.setUserCompanyPosition("Programmer");
         assertEquals("Developer", testUser.getUserCompanyRole());
     }
 
     @Test
     public void setDepartmentIdCorrectDepartmentId()throws Exception{
-        User testUser = setUpUser();
+        User testUser = setUser();
         testUser.setDepartmentId(2);
         assertEquals(1, testUser.getDepartmentId());
     }
@@ -71,11 +71,11 @@ public class UserTest {
 
 
 
-    public User setupUser (){
+    public User setUser (){
         return new User("Fredrick Angose", "Manager", "Developer", 1);
     }
 
-    public User setupAltUser (){
+    public User setAltUser (){
         return new User("Fredrick Angose", "Manager", "Developer", 1);
     }
 }
