fc41aebbb90a1219e261b2f58fbf81b9c4b47319
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-08-22 23:02:02 +0300

    Create NewsDao and enable adding and removing news feature

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,27 @@
 package dao;
+import models.*;
+import java.util.List;
 
-public class NewsDao {
+public interface NewsDao {
+
+    //create
+    void add(News news);
+    void addNewsToDepartment(News news, Department department);
+
+    //read
+    List<News> getAll();
+    News findById(int id);
+    List<News> getAllNewsByDepartment(int departmentId);
+    //
+    List<News> getNewsByDepartment(int departmentId);
+    //
+
+    //update
+    void update(int id, String newsTitle, String newsContent,int departmentId);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
 }
+
